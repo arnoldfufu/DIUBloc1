790e578216657294c68da44e2abbe8c1dfe97427
Olivier <olivier.elophe@gmail.com>
2019-04-26 15:51:38 +0200

    Classe carte ok

--- carte_creusee_POO.py
+++ carte_creusee_POO.py
@@ -56,139 +56,117 @@ class Carte:
             for y in range(self.hauteur // 2):
                 somme += self.carte[2 * y + 1][2 * x + 1]
         return somme
+
+    def destinations_possibles(self, x, y):
+        liste = []
+        if x + 2 < self.largeur - 1 and self.carte[y][x + 2] > 0:
+            liste.append(0)
+        if x - 2 > 0 and self.carte[y][x - 2] > 0:
+            liste.append(2)
+        if y + 2 < self.hauteur - 1 and self.carte[y + 2][x] > 0:
+            liste.append(3)
+        if y - 2 > 0 and self.carte[y - 2][x] > 0:
+            liste.append(1)
+        return liste
+
+    def copie_carte(self, c):
+        copie = []
+        for ligne in range(self.hauteur):
+            copie.append([])
+            for colonne in range(self.largeur):
+                copie[ligne].append(c[ligne][colonne])
+        return copie
+
+    def nouveau_chemin_depuis(self, x, y, t):
+        self.carte[y][x] = 0
+        liste = self.destinations_possibles(x, y)
+        if len(liste) == 0:
+            # creuse encore un coup pour débugger
+            # creuseAutour(carte,x,y,largeur,hauteur)
+            return
+        else:
+            choix = choice(liste)
+            if choix == 0:
+                self.carte[y][x + 1] = 0
+                self.carte[y][x + 2] = 0
+                x, y = x + 2, y
+            elif choix == 2:
+                self.carte[y][x - 1] = 0
+                self.carte[y][x - 2] = 0
+                x, y = x - 2, y
+            elif choix == 1:
+                self.carte[y - 1][x] = 0
+                self.carte[y - 2][x] = 0
+                x, y = x, y - 2
+            else:
+                self.carte[y + 1][x] = 0
+                self.carte[y + 2][x] = 0
+                x, y = x, y + 2
+            # basculer
+            self.affichage_graphique()
+            if t > 0:
+                self.nouveau_chemin_depuis(x, y, t - 1)
+            else:
+                return
+    def new_carte(self):
+        """génère le lab"""
+        if self.nb_cases_encore_a_creuser() > 0:
+            x0, y0 = self.origine_initiale()
+            t = ((self.largeur) // 2 * (self.hauteur) // 2) // 3
+            self.nouveau_chemin_depuis(x0, y0, t)
+        # afficheCarte(carte,largeur,hauteur)
+        while self.nb_cases_encore_a_creuser() > 0:
+            self.nouveau_chemin_jusque_zero_pas_dans_chemin_en_cours(x0, y0)
+            # afficheCarte(carte,largeur,hauteur)
+        return self.carte
+
+    def nouveau_chemin_jusque_zero_pas_dans_chemin_en_cours(self, x0, y0):
+        x, y = x0, y0  # pour rentrer dans while
+        while self.carte[y][x] == 0:
+            x, y = self.origine_initiale()
+        ancienne = self.copie_carte(self.carte)
+        while self.carte[y][x] > 0 or not ancienne[y][x] == 0:
+            self.carte[y][x] = 0
+            # print("debut chemin en",x,y)
+            # afficheCarte(carte,largeur,hauteur)
+            liste = self.destinations_possibles_avec_zero(x, y)
+            choix = choice(liste)
+            # print(choix)
+            if choix == 0:
+                self.carte[y][x + 1] = 0
+                x, y = x + 2, y
+            elif choix == 2:
+                self.carte[y][x - 1] = 0
+                x, y = x - 2, y
+            elif choix == 1:
+                self.carte[y - 1][x] = 0
+                x, y = x, y - 2
+            else:
+                self.carte[y + 1][x] = 0
+                x, y = x, y + 2
+            # basculer
+            self.affichage_graphique()
+
+    def destinations_possibles_avec_zero(self, x, y):
+        liste = []
+        if x + 2 < self.largeur - 1:
+            liste.append(0)
+        if x - 2 > 0:
+            liste.append(2)
+        if y + 2 < self.hauteur - 1:
+            liste.append(3)
+        if y - 2 > 0:
+            liste.append(1)
+        return liste
+    def affichage_graphique(self):
+        """Affichage graphique"""
+        for l in range(self.hauteur):
+            for c in range(self.largeur):
+                pygame.draw.rect(fenetre, (0, 0, 255 * self.carte[l][c]), (taille * c, taille * l, taille, taille), 0)
+        pygame.display.flip()
+        pygame.time.delay(200)
 #### FIN VErsion POO
 ##########################################################################################
-def bascule():
-    global carte
-    for l in range(hauteur):
-        for c in range(largeur):
-            pygame.draw.rect(fenetre, (0, 0, 255 * carte[l][c]), (taille * c, taille * l, taille, taille), 0)
-    pygame.display.flip()
-    pygame.time.delay(200)
-
-def copieCarte(c, largeur, hauteur):
-    copie = []
-    for ligne in range(hauteur):
-        copie.append([])
-        for colonne in range(largeur):
-            copie[ligne].append(c[ligne][colonne])
-    return copie
-def origineInitiale(carte, largeur, hauteur):
-    """ Point de départ aléatoire"""
-    x = randrange(largeur // 2)
-    y = randrange(hauteur // 2)
-    return 2 * x + 1, 2 * y + 1
-
-def nbreCasesEncoreACreuser(carte, largeur, hauteur):
-    somme = 0
-    for x in range(largeur // 2):
-        for y in range(hauteur // 2):
-            somme += carte[2 * y + 1][2 * x + 1]
-    return somme
-
-def destinationsPossibles(carte, x, y, largeur, hauteur):
-    liste = []
-    if x + 2 < largeur - 1 and carte[y][x + 2] > 0:
-        liste.append(0)
-    if x - 2 > 0 and carte[y][x - 2] > 0:
-        liste.append(2)
-    if y + 2 < hauteur - 1 and carte[y + 2][x] > 0:
-        liste.append(3)
-    if y - 2 > 0 and carte[y - 2][x] > 0:
-        liste.append(1)
-    return liste
-
-def destinationsPossiblesAvecZero(carte, x, y, largeur, hauteur):
-    liste = []
-    if x + 2 < largeur - 1:
-        liste.append(0)
-    if x - 2 > 0:
-        liste.append(2)
-    if y + 2 < hauteur - 1:
-        liste.append(3)
-    if y - 2 > 0:
-        liste.append(1)
-    return liste
-
-def nouveauCheminDepuis(carte, x, y, largeur, hauteur, t):
-    carte[y][x] = 0
-    liste = destinationsPossibles(carte, x, y, largeur, hauteur)
-    if len(liste) == 0:
-        # creuse encore un coup pour débugger
-        # creuseAutour(carte,x,y,largeur,hauteur)
-        return
-    else:
-        choix = choice(liste)
-        if choix == 0:
-            carte[y][x + 1] = 0
-            carte[y][x + 2] = 0
-            x, y = x + 2, y
-        elif choix == 2:
-            carte[y][x - 1] = 0
-            carte[y][x - 2] = 0
-            x, y = x - 2, y
-        elif choix == 1:
-            carte[y - 1][x] = 0
-            carte[y - 2][x] = 0
-            x, y = x, y - 2
-        else:
-            carte[y + 1][x] = 0
-            carte[y + 2][x] = 0
-            x, y = x, y + 2
-        # basculer
-        bascule()
-        if t > 0:
-            nouveauCheminDepuis(carte, x, y, largeur, hauteur, t - 1)
-        else:
-            return
-
-# ne marche pas
-def nouveauCheminJusqueZeroPasDansCheminEnCours(carte, x0, y0, largeur, hauteur):
-    x, y = x0, y0  # pour rentrer dans while
-    while carte[y][x] == 0:
-        x, y = origineInitiale(carte, largeur, hauteur)
-    ancienne = copieCarte(carte, largeur, hauteur)
-    while carte[y][x] > 0 or not ancienne[y][x] == 0:
-        carte[y][x] = 0
-        # print("debut chemin en",x,y)
-        # afficheCarte(carte,largeur,hauteur)
-        liste = destinationsPossiblesAvecZero(carte, x, y, largeur, hauteur)
-        choix = choice(liste)
-        # print(choix)
-        if choix == 0:
-            carte[y][x + 1] = 0
-            x, y = x + 2, y
-        elif choix == 2:
-            carte[y][x - 1] = 0
-            x, y = x - 2, y
-        elif choix == 1:
-            carte[y - 1][x] = 0
-            x, y = x, y - 2
-        else:
-            carte[y + 1][x] = 0
-            x, y = x, y + 2
-        # basculer
-        bascule()
-
-
-def newCarte(largeur, hauteur):
-    global carte
-    carteobjet = Carte(largeur, hauteur)
-    carteobjet.affiche_carte()
-    carte = carteobjet.carte
-    ##
-
-    if carteobjet.nb_cases_encore_a_creuser() > 0:
-        x0, y0 = carteobjet.origine_initiale()
-        t = ((largeur) // 2 * (hauteur) // 2) // 3
-        nouveauCheminDepuis(carte, x0, y0, largeur, hauteur, t)
-    # afficheCarte(carte,largeur,hauteur)
-    while carteobjet.nb_cases_encore_a_creuser() > 0:
-        nouveauCheminJusqueZeroPasDansCheminEnCours(carte, x0, y0, largeur, hauteur)
-        # afficheCarte(carte,largeur,hauteur)
-    return carte
-
-
 def newCarteAvecDepartEtArrivee(largeur, hauteur):
     nouvelle = newCarte(largeur, hauteur)
     x, y = 2 * randrange(largeur // 2) + 1, 0
@@ -197,8 +175,11 @@ def newCarteAvecDepartEtArrivee(largeur, hauteur):
     nouvelle[y][x] = 4252
     return nouvelle
 
+carteobjet = Carte(largeur, hauteur)
+carteobjet.new_carte()
 
-carte = newCarte(largeur, hauteur)
+
+
 
 pygame.quit()
 
